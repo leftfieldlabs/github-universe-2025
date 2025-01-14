6cb4d22fca6bd3ffa4a9a930f1fb98bdb1554dd0
Keren Megory-Cohen <keren@leftfieldlabs.com>
2025-01-14 16:29:14 -0600

    Fixes vite rollup config for gsap

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["gsap"],
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
-})
+});
